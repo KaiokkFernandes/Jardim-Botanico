8aa42df7e63f886a024cfd3e037ae7ad2b35ccf2
KaiokkFernandes <kaiofernandess654@gmail.com>
2025-05-05 20:38:59 -0300

    Alterar o roteador de BrowserRouter para HashRouter no App.js

--- jardim-botanico/src/App.js
+++ jardim-botanico/src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PlantPage from "./pages/plantPage";
 import Layout from "./components/layoutPage";
-
-
 
 function App() {
   return (
